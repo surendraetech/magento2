b01bbadfd9617d0aeeac8143decaf32a85326d0f
surendra <surendra@excellencetechnologies.in>
2016-04-07 11:47:15 +0530

    uploade new module test for magento 2

---
+++ Excellence/Test/Controller/Test/index.php
@@ -0,0 +1,20 @@
+<?php
+namespace Excellence\Test\Controller\Test;
+ 
+ 
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+         \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {    $this->resultPageFactory = $resultPageFactory;
+        return parent::__construct($context);
+    }
+     
+    public function execute()
+    {   $this->_view->loadLayout();
+        $this->_view->renderLayout();
+     // return $this->resultPageFactory->create(); 
+    } 
+}
+
